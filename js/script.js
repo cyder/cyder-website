64bb773ac75da35cc2c5806294aad7cd18e6d7da
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-05-31 17:42:19 +0900

    ページトップへ戻るボタン実装

--- js/script.js
+++ js/script.js
@@ -2,4 +2,8 @@ $(function() {
   $('#menu-button').on('click', function() {
     $('#nav').toggleClass('open');
   });
+
+  $('#page-top').on('click', function() {
+    $('body,html').animate({ scrollTop: 0 }, 200);
+  });
 });
