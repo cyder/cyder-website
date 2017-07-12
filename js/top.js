262ee91bd26b6985c9e299e0c0b77bfeb4e20b8f
rururuuko <honey.adon@gmail.com>
2017-07-12 21:45:26 +0900

    jsファイルの分割

---
+++ js/top.js
@@ -0,0 +1,34 @@
+$(function(){
+ var changeHeaderTranspearent = function() {
+    if($('#nav').hasClass('open')) {
+      $('#main-header').removeClass('transparent');
+    } else {
+      var obj_t_pos = $('#main-content').offset().top - $('#main-header').outerHeight(); //位置設定
+      var scr_count = $(window).scrollTop();  //スクロールした量
+      if(scr_count >= obj_t_pos) {
+        $('#main-header').removeClass('transparent');
+      } else {
+        $('#main-header').addClass('transparent');
+      }
+    }
+  };
+
+  changeHeaderTranspearent();
+
+  $('#menu-button').on('click', function() {
+    changeHeaderTranspearent();
+  });
+
+  $('#page-top').on('click', function() {
+    $('body,html').animate({ scrollTop: 0 }, 200);
+  });
+
+  $('#scroll-down').on('click', function() {
+    $('body,html').animate({ scrollTop: $('#main-content').offset().top - $('#main-header').outerHeight()}, 400);
+  });
+
+  $(window).scroll(function() {
+    changeHeaderTranspearent();
+  });
+
+});
